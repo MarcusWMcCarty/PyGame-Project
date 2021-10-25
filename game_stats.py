6b18e79df1adb10561d368d3b92e0d3b5c40d116
Marcus McCarty <marcus_mccarty1@baylor.edu>
2021-10-24 21:43:23 -0500

    High Score

--- game_stats.py
+++ game_stats.py
@@ -9,6 +9,10 @@ class GameStats:
         #Start game in an inactive State.
         self.game_active = False
 
+        #High score should never be reset
+        self.high_score = 0
+        
+
 
     def reset_stats(self):
         """Initialize stats that can change during the game."""

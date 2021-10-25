d4d29468c442c21272df97bc7f1e0801ceb37449
Marcus McCarty <marcus_mccarty1@baylor.edu>
2021-10-24 19:13:59 -0500

    Detecting Ship Collisions

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ class Settings:
         self.ship_speed = 1.5
 
         #Bullet Settings
-        self.bullet_speed = 1.5
+        self.bullet_speed = 10
         self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
@@ -20,7 +20,7 @@ class Settings:
 
         #Alien Settings
         self.alien_speed = 1.0 
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 100
         #Fleet_direction of 1 represents right and -1 represents left
         self.fleet_direction = 1
 

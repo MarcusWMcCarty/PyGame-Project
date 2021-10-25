419a81817a683c3b703e9fb35863919ac7ba8a74
Marcus McCarty <marcus_mccarty1@baylor.edu>
2021-10-24 20:41:27 -0500

    Creating ScoreBoard

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ from bullet import Bullet
 from alien import Alien
 from game_stats import GameStats
 from button import Button
+from scoreboard import Scoreboard
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -23,8 +24,11 @@ class AlienInvasion:
             self.settings.screen_height = self.screen.get_rect().height
             pygame.display.set_caption("Alien Invasion")
 
-            #Create an instance to store game statistics
+
+            #Create an instance to store game statistics and score
             self.stats = GameStats(self)
+            self.sb = Scoreboard(self)
+
 
             self.ship = Ship(self)
             self.bullets = pygame.sprite.Group()
@@ -72,7 +76,7 @@ class AlienInvasion:
         if button_clicked and not self.stats.game_active:
             #Reset the game settings
             self.settings.initialize_dynamic_settings()
-            
+
             #Reset the game stats
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -237,6 +241,9 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        #Draw the score information 
+        self.sb.show_score()
 
         #Draw the play button if the game is inactive
         if not self.stats.game_active:
--- game_stats.py
+++ game_stats.py
@@ -5,7 +5,7 @@ class GameStats:
         """Initialize Statistics"""
         self.settings = ai_game.settings
         self.reset_stats()
-        
+
         #Start game in an inactive State.
         self.game_active = False
 
@@ -13,3 +13,4 @@ class GameStats:
     def reset_stats(self):
         """Initialize stats that can change during the game."""
         self.ships_left = self.settings.ship_limit
+        self.score = 0

4f3d7d6b0797bbd9af3c3d66c28edd91819cfed8
Marcus McCarty <marcus_mccarty1@baylor.edu>
2021-10-24 20:22:57 -0500

    Leveling up Speed Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -70,6 +70,9 @@ class AlienInvasion:
         """Start a new game when the play clicks Play"""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            #Reset the game settings
+            self.settings.initialize_dynamic_settings()
+            
             #Reset the game stats
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -223,6 +226,7 @@ class AlienInvasion:
             #Destory exisiting bullets and create a new fleet
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
 
     def _update_screen(self):
